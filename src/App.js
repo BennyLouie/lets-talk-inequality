22d9abd944e41be1e2e2a978a0d0156fbdf272c5
BennyLouie <bennylouie1412@gmail.com>
2019-11-07 12:43:54 -0500

    added withRouter

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import GenderGapFacts from './components/GenderGapFacts';
 import AgeGapFacts from './components/AgeGapFacts';
 import RacialGapFacts from './components/RacialGapFacts';
 import './App.css';
-import { Route, Switch, NavLink, Redirect } from 'react-router-dom'
+import { Route, Switch, NavLink, Redirect, withRouter } from 'react-router-dom'
 
 class App extends Component {
   render() {
